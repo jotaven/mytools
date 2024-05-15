654dcdadd775f781cb45898ce1202e885a4d8009
João Victor Nunes de Moura <jotaven.moura@gmail.com>
2024-05-15 15:40:03 -0300

    bug fix: Static Files missing

--- app/core/settings.py
+++ app/core/settings.py
@@ -120,9 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATIC_ROOT = "vol/static"
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+    STATIC_ROOT = "static"
 
 
 MEDIA_URL = '/media/'
